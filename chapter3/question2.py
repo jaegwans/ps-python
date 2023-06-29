b85c61869823d0d171cc5cdd5bb31cccb9f02ddb
jaegwans <skhu201811140@gmail.com>
2023-06-29 22:58:06 +0900

    feat: q3-2

---
+++ chapter3/question2.py
@@ -0,0 +1,11 @@
+m,n = list(map(int,input().split()))
+a1 = list(map(int,input().split()))
+a2 = list(map(int,input().split()))
+a3 = list(map(int,input().split()))
+arr = []
+print(m,n)
+arr.append(min(a1))
+arr.append(min(a2))
+arr.append(min(a3))
+# m 행 n 열
+print(max(arr))
