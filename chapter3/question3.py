db7ccc76a2f75fb04aa260d7c3eec1496cf432dd
jaegwans <skhu201811140@gmail.com>
2023-06-30 10:07:51 +0900

    feat: q3-3

---
+++ chapter3/question3.py
@@ -0,0 +1,22 @@
+n,k = list(map(int,input().split()))
+count = 0
+
+# n 은 어떤 수 k는 나눌 수
+
+while True:
+  
+    if(n==1):
+        break
+    count += 1
+    if(n%k==0):
+        n = n/k
+    else:
+        n -= 1
+
+print(count)
+    
+# 나누어지지 않으면 뺴고
+# 나누어지면 나눈다.
+# 횟수를 카운팅
+
+#ex 25 5- > 7
